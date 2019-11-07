e4546099b878a0edc2c52d1300685dd890246cbc
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-11-07 17:15:04 -0600

    Get done, initial commit

--- users/userRouter.js
+++ users/userRouter.js
@@ -10,13 +10,30 @@ router.post('/:id/posts', (req, res) => {
 
 });
 
-router.get('/', (req, res) => {
+//Get
+router.get('/', async (req, res) => {
+    try {
+        const posts = await posts.find(req.query);
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({
+            message:"Error retrieving posts."
+        })
+    }
+    });
 
-});
-
-router.get('/:id', (req, res) => {
-
-});
+    router.get('/:id', (req, res) => {
+        try {
+            const post = await Posts.findById(req.params.id);
+            if(post) {
+                req.status(200).json(post);
+            } else {
+                res.status(404).json({message:"Post not found"});
+                }
+            } catch(error) {
+                res.status(500).json({message:"Error retrieving post"})
+          }
+        })
 
 router.get('/:id/posts', (req, res) => {
 
