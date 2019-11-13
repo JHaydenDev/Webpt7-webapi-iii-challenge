f0e56193eaeb06682bbf5533be55737da3da2cc6
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-11-13 10:49:17 -0600

    userRouter done.

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,14 +1,22 @@
 const express = 'express';
 
 const router = express.Router();
+router.post("/", validateUser, (req, res) => {
+    res.status(201).json(req.user);
+  });
+  
 
-router.post('/', (req, res) => {
-
-});
-
-router.post('/:id/posts', (req, res) => {
-
-});
+router.post("/:id/posts", validatePost, validateUserId, (req, res) => {
+    const post = req.post;
+    console.log(post);
+    Posts.insert(post)
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "POST ERROR" });
+      });
+  });
 
 //Get
 router.get('/', async (req, res) => {
@@ -17,11 +25,12 @@ router.get('/', async (req, res) => {
         res.status(200).json(posts);
     } catch (error) {
         res.status(500).json({
-            message:"Error retrieving posts."
+            message:"GET POST ERROR"
         })
     }
     });
 
+//Get:ID
     router.get('/:id', (req, res) => {
         try {
             const post = await Posts.findById(req.params.id);
@@ -31,7 +40,7 @@ router.get('/', async (req, res) => {
                 res.status(404).json({message:"Post not found"});
                 }
             } catch(error) {
-                res.status(500).json({message:"Error retrieving post"})
+                res.status(500).json({message:"GET POST ID ERROR"})
           }
         })
 
@@ -39,26 +48,103 @@ router.get('/:id/posts', (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+//Delete
+router.delete("/:id", validateUserId, (req, res) => {
+    const userId = req.user.id;
+    Users.remove(userId)
+      .then(user => {
+        res.status(200).json({ message: "USER DELETED" });
+      })
+      .catch(err => {
+        res.status(500).json({ message: "COULD NOT DELETE" });
+      });
+  });
+  
+  //Put
+router.put("/:id", validateUserId, (req, res) => {
+    const userBody = req.body;
+    const userId = req.user.id;
+    console.log(userId);
+  
+    if (!userBody.name) {
+      return res.status(400).json({ message: "ERROR ADD NAME" });
+    }
+  
+    //Update
+    Users.update(userId, userBody)
+      .then(user => {
+        if (user) {
+          res.status(200).json({ message: "USER UPDATED" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "USER WASNT UPDATED" });
+      });
+  });
+  
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-
-};
+    const id = req.params.id;
+    Users.getById(id)
+      .then(user => {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res.status(400).json({ message: "BAD USER ID" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "ID DOESNT EXIST" });
+      });
+  }
+  
 
 function validateUser(req, res, next) {
-
-};
+    const userBody = req.body;
+    console.log(userBody);
+  
+    if (Object.keys(userBody).length === 0) {
+      return res.status(400).json({ message: "NEED USER DATA" });
+    } else if (!userBody.name) {
+      return res.status(400).json({ message: "MISSING NAME" });
+    } else {
+      Users.insert(userBody)
+        .then(user => {
+          if (user) {
+            req.user = user;
+            next();
+          }
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ message: "USER ADDING ERROR" });
+        });
+    }
+  }
+  
 
 function validatePost(req, res, next) {
+    const postBody = { ...req.body, user_id: req.params.id };
 
-};
+    console.log("req.body", postBody);
+  
+    if (Object.entries(postBody).length === 0) {
+      res.status(400).json({ message: "NEED POST DATA" });
+    } else if (!postBody.text) {
+      res.status(400).json({ message: "NEED BODY TEXT" });
+    } else {
+      Posts.insert(postBody).then(post => {
+        if (post) {
+          console.log(postBody);
+          req.post = post;
+          next();
+        }
+      });
+    }
+  }
 
 module.exports = router;
